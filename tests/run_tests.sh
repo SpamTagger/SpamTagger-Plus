ffefc93b2b10ea9424bb1da387068999b82d65a8
John Mertz <git@john.me.tz>
2025-09-20 22:00:04 -0600

    (Re)enable epel repository
    After removing excess packages from actual image, this repository is no
    longer shipped. Add it again before trying to install Perl::Critic
    during `just test`

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# (Re)enable Enterprise Linux repo which contains Perl::Critic
+dnf -y install --setopt=install_weak_deps=False --allowerasing \
+  epel-release
+
+# Install Perl::Critic which provides `prove` with TAP compatible testing
 dnf -y install --setopt=install_weak_deps=False --allowerasing \
   perl-Perl-Critic
-  
+
 prove /usr/spamtagger/tests/
