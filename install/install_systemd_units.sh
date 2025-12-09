578427ab7ad56f1e2b09dc52e66a49692b5bd4e0
John Mertz <git@john.me.tz>
2025-12-09 13:35:00 -0700

    Basic script to link systemd units to /usr/lib

---
+++ install/install_systemd_units.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$SRCDIR" = "" ]; then
+  SRCDIR=$(grep 'SRCDIR' /etc/spamtagger.conf | cut -d ' ' -f3)
+  if [ "SRCDIR" = "" ]; then
+    SRCDIR=/var/spamtagger
+  fi
+fi
+
+for i in $(find /usr/spamtagger/scripts/systemd/* -maxdepth 0); do
+  if [[ -e "/usr/lib/systemd/system/$(basename $i)" ]]; then
+    echo $i already exists at /usr/lib/systemd/system/$(basename $i)
+    rm -rf /usr/lib/systemd/system/$(basename $i)
+  fi
+  ln -s $i /usr/lib/systemd/system/$(basename $i)
+done
+
+systemctl daemon-reload
