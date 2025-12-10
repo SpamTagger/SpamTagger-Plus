3801840bf15c631a125d7657e2cc1c77bcf2f20e
John Mertz <git@john.me.tz>
2025-12-10 15:04:43 -0700

    Table will remain as mysql_auth

--- install/dbs/t_cf_mysql_auth.sql
+++ install/dbs/t_cf_mysql_auth.sql
@@ -1,1 +1,14 @@
-./install/dbs/t_cf_mariadb_auth.sql
+use st_config;
+
+DROP TABLE IF EXISTS mysql_auth;
+
+CREATE TABLE mysql_auth (
+  username 			 varchar(200) NOT NULL,
+  domain                         varchar(50),
+  password			 varchar(200) NOT NULL,
+  email				 varchar(200) NOT NULL,
+  realname			 varchar(200),
+  id                             int(11) auto_increment not null,
+  PRIMARY KEY (id)
+);
+
